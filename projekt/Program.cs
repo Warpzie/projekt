a945813b6abfc7f79a68fd15600c7100836de1ff
ALEX <alex.why.would.you@gmail.com>
2024-12-03 20:13:36 +0100

    yay

--- projekt/Program.cs
+++ projekt/Program.cs
@@ -8,13 +8,13 @@ namespace projekt
     {
         static void Main(string[] args)
         {
-            StreamReader subor = new StreamReader("C:/Users/ALEX/source/repos/projekt/projekt/TextFile1.txt");
+            StreamReader sr = new StreamReader("C:/Users/ALEX/source/repos/projekt/projekt/TextFile1.txt");
             Console.WriteLine("zakodovat alebo dekodovat?");
             string vyber = Console.ReadLine();
 
             Console.WriteLine("aky je posun sifry?");
             int posun = int.Parse(Console.ReadLine());
-            string text = subor.ReadLine();
+            string text = sr.ReadLine();
             static string koduj(string text,int posun)
             {
                 string vysledok = "";
@@ -24,25 +24,40 @@ namespace projekt
                     if (c!=' ')
                     {
                         int index = c + posun % 256;
+
                         vysledok += (char)index;
                     }
                 }
                 return vysledok;
             }
+            static string dekoduj(string text,int posun)
+            {
+                string vysledok = "";
+                foreach (char c in text)
+                {
+                    if (c != ' ')
+                    {
+                        int index = c - posun % 256;
+
+                        vysledok += (char)index;
+                    }
+                }
+                return vysledok;
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("C:/Users/ALEX/source/repos/projekt/projekt/TextFile1.txt", false);
+
             if (vyber == "zakodovat")
             {
-                Console.WriteLine(koduj(text,posun));
-                
-
-                /* byte[] bytes = Encoding.ASCII.GetBytes(text);
-                int encoded = BitConverter.ToInt32(bytes, 0);
-                Console.WriteLine(encoded);
-                int kodovanie = encoded + posun;
-                string vysledok = Convert.ToChar(kodovanie);
-                Console.WriteLine(vysledok);
-                */
+                Console.WriteLine(koduj(text,posun) + "-zakodovane");
+                sw.WriteLine(koduj(text, posun) + "-zakodovane");
             }
-            
+            else if (vyber == "dekodovat")
+            {
+                Console.WriteLine(dekoduj(text,posun) + "-dekodovane");
+                sw.WriteLine(dekoduj(text, posun) + "-dekodovane");
+            }
+            sw.Close();
             
         }
     }
